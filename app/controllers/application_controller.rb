bb75be691de3f234ef8039c4cb2e1c0007d83269
katsuokun-2142 <b2a1t4t2lefield@gmail.com>
2024-03-26 17:04:37 +0900

    ユーザー管理機能正常系の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    # devise_parameter_sanitizerのpermitメソッド
+    # devise_parameter_sanitizer.permit(:deviseの処理名, keys: [:許可するキー])
+    # binding.pry
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :furi_last_name, :furi_first_name, :date_of_birth])
+  end
+
 end
---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :furi_last_name, presence: true
+  validates :furi_first_name, presence: true
+  validates :date_of_birth, presence: true
+end
