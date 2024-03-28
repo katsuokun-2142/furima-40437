56dd8367505a09ea7cddffe4f554543187029af5
katsuokun-2142 <b2a1t4t2lefield@gmail.com>
2024-03-28 18:37:21 +0900

    手数料と利益計算対応

---
+++ app/javascript/commission_profit_calc.js
@@ -0,0 +1,20 @@
+function commission_profit_calc (){
+  const item_price = document.getElementById('item-price')
+  console.log(item_price)
+  const tax = 0.1
+  item_price.addEventListener('keyup',(e)=>{
+    const value = item_price.value
+    console.log(value)
+    // 販売手数料
+    let commission = Math.floor(value * tax)
+    const add_tax_price = document.getElementById('add-tax-price')
+    add_tax_price.innerHTML = commission
+    // 販売利益
+    let profit = value - commission
+    const profit_price = document.getElementById('profit')
+    profit_price.innerHTML = profit
+  })
+};
+
+window.addEventListener('turbo:load', commission_profit_calc);
+ 
