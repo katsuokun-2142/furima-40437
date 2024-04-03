f45a7f67b0174c1f8927abdd0a88bf17394c544f
katsuokun-2142 <b2a1t4t2lefield@gmail.com>
2024-04-03 19:00:17 +0900

    Formオブジェクトによるパラメータ取得

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,21 @@
 class OrdersController < ApplicationController
   def index
+    @item = Item.find(params[:item_id])
+    @order_shipping_information = OrderShippingInformation.new# Formオブジェクトを追加
+  end
+
+  def create
+    binding.pry
+  end
+
+  private
+
+  def order_shipping_information_params
+    params.require(:order_shipping_information).permit(:zip_code,
+                                                        :state_province_id,
+                                                        :city_town_village,
+                                                        :street_address,
+                                                        :building_name,
+                                                        :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
---
+++ app/models/order_shipping_information.rb
@@ -0,0 +1,22 @@
+class OrderShippingInformation
+  include ActiveModel::Model
+  attr_accessor :zip_code, :state_province_id, :city_town_village, :street_address, :building_name, :phone_number, :order_id, :item_id, :user_id
+
+  # バリデーション
+  with_options presence: true do
+  end
+
+  def save
+    # 購入記録を保存し、変数orderに代入する
+    otder = Order.create(user_id: user_id, item_id: item_id)
+    # 発送先情報を保存する
+    # order_idには、変数orderのidと指定する
+    Address.create(zip_code: zip_code,
+                   state_province_id: state_province_id,
+                   city_town_village: city_town_village,
+                   street_address: street_address,
+                   building_name: building_name,
+                   phone_number: phone_number,
+                   order_id: order.id)
+  end
+end
