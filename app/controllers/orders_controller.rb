7228911fb7ce71a342c41ecb95e0a55e3f30d028
katsuokun-2142 <b2a1t4t2lefield@gmail.com>
2024-04-04 20:58:19 +0900

    クレジットカード機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,7 @@ class OrdersController < ApplicationController
     if Order.exists?(item_id: params[:item_id])
       redirect_to root_path and return
     end
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @item = Item.find(params[:item_id])
     @order_shipping_information = OrderShippingInformation.new
   end
@@ -15,10 +16,14 @@ class OrdersController < ApplicationController
   def create
     @order_shipping_information = OrderShippingInformation.new(order_shipping_information_params)
     if @order_shipping_information.valid?
+      pay_item
+      # 購入記録と発送先情報の登録
       @order_shipping_information.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       @item = Item.find(params[:item_id])
+      @order_shipping_information = OrderShippingInformation.new(order_shipping_information_params)
       render :index, status: :unprocessable_entity
     end
   end
@@ -31,6 +36,19 @@ class OrdersController < ApplicationController
                                                         :city_town_village,
                                                         :street_address,
                                                         :building_name,
-                                                        :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+                                                        :phone_number).merge(user_id: current_user.id,
+                                                                              item_id: params[:item_id],
+                                                                              token: params[:token])
+  end
+
+  def pay_item
+    # クレジットカード機能
+    target_item = Item.find(params[:item_id]) # 商品情報取得
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount:   target_item.selling_price,  # 商品の値段
+      card:     order_shipping_information_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/order_shipping_information.rb
+++ app/models/order_shipping_information.rb
@@ -1,18 +1,19 @@
 class OrderShippingInformation
   include ActiveModel::Model
   attr_accessor :zip_code, :state_province_id, :city_town_village, :street_address, :building_name, :phone_number, :order_id, :item_id, :user_id
+  attr_accessor :token
 
   # バリデーション
   with_options presence: true do
     # 購入記録
     validates :item_id, :user_id
     # 発送先情報
-    validates :zip_code, format: {with: /\A\d{3}-\d{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :phone_number, format: {with: /\A\d{10,11}\z/, message: "is invalid. Does not contain a hyphen (-)"}
+    validates :zip_code,                          format: {with: /\A\d{3}-\d{4}\z/, message: "is invalid. Include hyphen(-), for example 999-9999."}
+    validates :phone_number,                      format: {with: /\A\d{10,11}\z/, message: "is invalid. Does not contain a hyphen (-)"}
+    validates :state_province_id,                 numericality: { other_than: 1, message: "is not selected" } 
     validates :city_town_village, :street_address
+    validates :token
   end
-  # 発送先情報
-  validates :state_province_id, numericality: { other_than: 1, message: "can't be blank" } 
   # 購入記録
   validate :item_must_be_unique
 
@@ -31,7 +32,7 @@ class OrderShippingInformation
   end
 
   private
-
+  
   def item_must_be_unique
     if Order.exists?(item_id: item_id)
       errors.add(:base, "商品はすでに売り切れです。")
